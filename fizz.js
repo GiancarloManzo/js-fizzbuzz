6ee48c70ba9591922116abd25bb907221a20d9a3
Giancarlo <cooper458.gm@gmail.com>
2025-12-17 13:41:25 +0100

    added prompt

--- fizz.js
+++ fizz.js
@@ -1,4 +1,11 @@
+const numero = parseInt(prompt("Inserisci un numero"));
+
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
   }
 }
